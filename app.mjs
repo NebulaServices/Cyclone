1b01485b47470dfac18e6cf49b9890d06b8b49f9
SmartCoder3000 <83769295+SmartCoder3000@users.noreply.github.com>
2022-06-15 15:49:08 -0400

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,31 +1,104 @@
-import http from 'http';
-import * as fs from 'fs';
-import * as bare from './static/customBare.js';
-import path from 'path';
+const fs = require('fs');
+const fetch = require('node-fetch');
 
-const __dirname = process.cwd();
+const config = {
+  prefix: "/service",
+  port: null
+}
 
-const port = 8080;
+function rewriteJavascript(js) {
+  var javascript = js.replace('window.location', 'document._dlocation')
+  javascript = javascript.replace('document.location', 'document._dlocation')
+  javascript = javascript.replace('location.', 'document._location.')
+  return javascript
+}
 
-const requestListener = function(req, res) {
-  if (bare.isBare(req, res)) {
-    bare.route(req,res);
-  } else {
-    if (req.url === "/") {
-      var index = fs.readFileSync(__dirname+"/static/index.html", 'ascii');
-      res.end(index)
-    } else if (req.url === "/cyclone.svg") {
-      var image = fs.readFileSync(__dirname+"/static/cyclone.png");
-      res.writeHead(200,'Sucess', {
-        'content-type':'image/png'
-      })
-      res.end(image)
-    } else {
-      res.writeHead(404)
-      res.end("404 Not Found")
+function insertScript(html, origin) {
+  var res = `<!DOCTYPE html>
+<html>
+<head>
+<script preload type="module" src="/cyclone.js"></script>
+</head>
+<body>
+${html}
+</body>
+</html>`
+  return res
+} // 
+
+async function fetchBare(url, res, req) {
+  try {
+    var origin = 'https' + "://" + req.rawHeaders[1];
+
+    var options = {
+      method: req.method,
+      headers: {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36",
+        cookies: req.cookies
+      },
+      credentials: "same-origin"
     }
+
+    var request = await fetch(url.href, options);
+    var contentType = request.headers.get('content-type') || 'application/javascript'
+    var output = null;
+
+    if (contentType.includes('html') || contentType.includes('javascript')) {
+      var doc = await request.text();
+    }
+
+    res.writeHead(200, "Sucess", {
+      "content-type": contentType
+    })
+
+    if (contentType.includes('html')) {
+      output = insertScript(doc, origin);
+      res.write(output);
+      res.end();
+    } else if (contentType.includes('javascript')) {
+      output = rewriteJavascript(doc)
+      res.write(output);
+      res.end()
+    } else {
+      request.body.pipe(res)
+    }
+
+
+  } catch (e) {
+    console.log(e);
+
+    res.end(e)
   }
 }
 
-const server = http.createServer(requestListener);
-server.listen(port);
+async function route(req, res) {
+  var path = req.url;
+
+  if (path.startsWith(config.prefix + "/")) {
+
+    try {
+      var url = new URL(path.split(config.prefix + "/")[1])
+    } catch {
+      var url = new URL("https://" + path.split(config.prefix + "/")[1])
+    }
+
+    fetchBare(url, res, req);
+
+  } else if (path.startsWith('/cyclone.js')) {
+    var cyclone = fs.readFileSync(__dirname + '/static/cyclone.js', 'utf8');
+    res.writeHead(200, "Sucess", {
+      "content-type": "application/javascript"
+    })
+    res.write(cyclone);
+    res.end();
+  }
+}
+
+function isBare(req, res) {
+  return (req.url.startsWith(config.prefix));
+}
+
+export {
+  route,
+  isBare
+}
